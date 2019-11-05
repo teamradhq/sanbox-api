171f7c44672f2d7381032ccf939784d0b3f79830
Paul Beynon <paul.beynon@gmail.com>
2019-11-05 15:14:36 +1100

    JsonStore should fail if storepath doesn't exist

--- src/lib/JsonStore/JsonStore.js
+++ src/lib/JsonStore/JsonStore.js
@@ -1,15 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
 import getJsonStore from './getJsonStore';
 import setJsonStore from './setJsonStore';
 
 const cached = Symbol.for('JsonStoreCachedResults');
 const cachedPath = Symbol.for('JsonStoreCachePath');
 
+/**
+ * Use JsonStore to store a mock resource collection
+ * and store it in a JsonFile.
+ */
 class JsonStore {
+  /**
+   * Set the location of JsonStore file.
+   * @param  {String} storepath Full path to json file.
+   */
   constructor(storepath) {
-    if (!storepath) {
-      throw Error('A store path is required');
+    if (!fs.existsSync(path.dirname(storepath))) {
+      throw Error('Store path should point to an existing location');
     }
-    
+
     this[cachedPath] = storepath;
   }
 
@@ -26,6 +37,7 @@ class JsonStore {
   }
 
   set data(payload) {
+    this[cached] = payload;
     setJsonStore(this[cachedPath], payload);
   }
 }
--- tests/lib/JsonStore/JsonStore.test.js
+++ tests/lib/JsonStore/JsonStore.test.js
@@ -1,11 +1,20 @@
 /* eslint-disable no-unused-vars */
 
+import * as fs from 'fs';
+fs.default.existsSync = jest.fn(bool => bool !== false);
+
 import JsonStore from '@/lib/JsonStore';
 import * as getJsonStore from '@/lib/JsonStore/getJsonStore';
 import * as setJsonStore from '@/lib/JsonStore/setJsonStore';
 
-getJsonStore.default = jest.fn(() => [1]);
-setJsonStore.default = jest.fn();
+const get = jest.fn(() => [1]);
+const set = jest.fn();
+
+getJsonStore.default = get;
+setJsonStore.default = set;
+
+const cached = Symbol.for('JsonStoreCachedResults');
+const cachedPath = Symbol.for('JsonStoreCachePath');
 
 describe('JsonStore', () => {
   let store;
@@ -14,22 +23,44 @@ describe('JsonStore', () => {
     store = new JsonStore('test-file-path');
   });
 
-  it('should throw error if storepath is not passed', () => {
-    expect(() => new JsonStore()).toThrow();
-  });
+  describe('should throw if storepath', () => {
+    it('is not passed', () => {
+      expect(() => new JsonStore()).toThrow();
+    });
 
-  it('should get json store from filesystem', () => {
-    const { data } = store;
-    expect(getJsonStore.default).toBeCalled();
-  });
-
-  it('should set json store to filesystem', () => {
-    store.data = {};
-    expect(setJsonStore.default).toBeCalled();
+    it('directory does not exist', () => {
+       expect(() => new JsonStore(false)).toThrow();   
+    });
   });
 
   it('should have a length of 1', () => {
     const { data } = store;
-    expect(store.length).toBe(1);
+    expect(store.length).toBe(get().length);
+  });
+
+  describe('get data', () => {
+    it('should get from filesystem', () => {
+      const { data } = store;
+      expect(getJsonStore.default).toBeCalled();
+    });
+
+    it('should get from cache', () => {
+      const a = store.data;
+      const b = store.data;
+      expect(getJsonStore.default).toBeCalledTimes(1);
+    });
+  });
+
+  describe('set data', () => {
+    it('should update cache', () => {
+      const expected = { foo: 'bar' };
+      store.data = expected;
+      expect(store[cached]).toEqual(expected);
+    });
+
+    it('should store to filesystem', () => {
+      store.data = {};
+      expect(setJsonStore.default).toBeCalled();
+    });
   });
 });
